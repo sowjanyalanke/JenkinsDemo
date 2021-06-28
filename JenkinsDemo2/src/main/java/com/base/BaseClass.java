632ed5191428c8a1d197336d59443a60770bbd83
sowjanyalanke <53407865+sowjanyalanke@users.noreply.github.com>
2021-06-28 14:04:29 +0530

    Update BaseClass.java

--- JenkinsDemo2/src/main/java/com/base/BaseClass.java
+++ JenkinsDemo2/src/main/java/com/base/BaseClass.java
@@ -14,20 +14,20 @@ public class BaseClass {
 	
 	@BeforeMethod
 	public void setup() {
-// 		if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
-// 			WebDriverManager.chromedriver().setup();
-// 			driver = new ChromeDriver();
-// 		} else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
-// 			WebDriverManager.firefoxdriver().setup();
-// 			driver = new FirefoxDriver();
-// 		} else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
-// 			WebDriverManager.iedriver().setup();
-// 			driver = new InternetExplorerDriver();
-// 		}
-// 		driver.manage().window().maximize();
-// 		driver.get(System.getProperty("url"));
-		WebDriverManager.firefoxdriver().setup();
-		driver=new FirefoxDriver();
+		if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver();
+		}
+		driver.manage().window().maximize();
+		driver.get(System.getProperty("url"));
+// 		WebDriverManager.firefoxdriver().setup();
+// 		driver=new FirefoxDriver();
 			driver.get("https://www.google.com/");
 			driver.manage().window().maximize();
 	}	
